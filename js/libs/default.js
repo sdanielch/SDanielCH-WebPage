9686ae49da5ee13d134691da3e36eb2890b48ace
sdanielch <sdanielch@gmail.com>
2016-01-04 17:31:45 +0100

    update

--- js/libs/default.js
+++ js/libs/default.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
 
+$(".fbutton").click( function() {
+  // alert("Probando");
+//$(".main3").fadeOut(500).wait(1000).fadeIn(300);
 
+$( ".main" ).animate({
+    "max-height": "800px"
+  }, 1000, function() {
+    // Animation complete.
+  });
+
+
+});
 
 });
